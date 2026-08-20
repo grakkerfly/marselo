9d6745835a2cfde19c5970a2f7c30f5adcacda33
grakkerfly <grakkerfly@outlook.com>
2026-08-20 19:45:38 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 const CONFIG = {
   room: 'assets/room.glb',
   links: {
-    pumpFun: '', // Adult-managed external trading URL, intentionally disabled by default.
+    pumpFun: 'https://pump.fun/coin/',
     twitter: 'https://x.com/marselomoon',
     wiki: 'https://herofanon.fandom.com/wiki/Marselo'
   },
